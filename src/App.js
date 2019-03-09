32ef131d1d3d9a09e71086600864b958cb972ab2
Your Name <you@example.com>
2019-03-09 15:34:05 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function User (props) {
     return (
-      <div className="container">
-        <tr>
-          <td className="row-1"><img src={props.user.picture.thumbnail}/></td>
-          <td className="">{props.user.name.first} {props.user.name.last}</td>
-          <td>{props.user.email}</td>
-          <td>{props.user.cell}</td>
-        </tr>
+      <div className="row row-body">
+        <div className="col-md-3 row-border"><img src={props.user.picture.thumbnail} alt="user-img"/></div>
+        <div className="col-md-3 row-border">{props.user.name.first} {props.user.name.last}</div>
+        <div className="col-md-3 row-border">{props.user.email}</div>
+        <div className="col-md-3 row-border">{props.user.cell}</div>
       </div>
     );
 }
@@ -22,7 +21,7 @@ class App extends Component{
     }
   }
   componentDidMount(){
-    axios.get('https://randomuser.me/api/?results=100')
+    axios.get('https://randomuser.me/api/?results=5')
     .then(response =>{ 
       this.setState({users: response.data.results})
       console.log(this.state.users)
@@ -31,18 +30,23 @@ class App extends Component{
   }
   render(){
     const users = this.state.users.map((user, index) => 
-      <div key={index}><User user={user}/></div>
+      <div key={index}><User user={user}/></div> 
     )
+    // const users = this.state.users.map(user,index => {
+    //   return (
+    //     <User user = {user}/>
+    //   );
+    // });
     return (
-      <thread>
-        <tr>
-          <th className="row-1">Image</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Phone</th>
-        </tr>
-        {users} 
-      </thread>
+      <div >
+        <div className="row header ">
+          <div className="col-sm-3 row-border">Image</div>
+          <div className="col-sm-3 row-border">Name</div>
+          <div className="col-sm-3 row-border">Email</div>
+          <div className="col-sm-3 row-border">Phone</div>
+        </div>
+        <div>{users}</div>
+      </div>
     );
   }
 }
